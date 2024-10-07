0516587031bd1132e060b480b287d9b771a2a035
siennaleegibbons <siennaleegibbons2005@gmail.com>
2024-10-07 09:54:07 +0100

    Display Message Changed

---
+++ my_first_code.py
@@ -0,0 +1,36 @@
+print ("Welcome to L4 Programming Unit")
+
+
+
+
+#f strings
+
+name = "Sienna"
+age = 19
+print(f"{name} is {age} years old")
+
+name = "Sienna"
+print("Hello" + name)
+print("{} is {} years old".format("Sienna", 19))
+
+print(342+699) #addition
+print(5022 - 901) #subtraction
+print(5 * 10) # multiplication
+print(604 / 4) #division
+print(3 % 2) #remainder
+
+
+#assigning
+a = 265
+b= 5924
+result = a + b
+print ("a + b =", result)
+
+a = (6 * 4)
+b = (5 ** 2 )
+c= (4 % 3)
+print = (a + b - c)
+
+
+
+
